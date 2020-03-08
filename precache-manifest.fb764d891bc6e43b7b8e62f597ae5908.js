d98afc38f8bb8d3d4f36fd8d779ccd994413061e
InKyoJeong <viapolar@naver.com>
2020-03-08 11:04:50 +0900

    Updates

---
+++ precache-manifest.fb764d891bc6e43b7b8e62f597ae5908.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc59f158a21d5b3a7c10393df9dc5d90",
+    "url": "/dku_festival/index.html"
+  },
+  {
+    "revision": "f3e1c0140dcd7242f670",
+    "url": "/dku_festival/static/css/main.3164e3a4.chunk.css"
+  },
+  {
+    "revision": "23f121558b4da79643c0",
+    "url": "/dku_festival/static/js/2.6192822e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/dku_festival/static/js/2.6192822e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e1c0140dcd7242f670",
+    "url": "/dku_festival/static/js/main.40b503e6.chunk.js"
+  },
+  {
+    "revision": "f1fa061910412fc72c96",
+    "url": "/dku_festival/static/js/runtime-main.78eda182.js"
+  },
+  {
+    "revision": "4bcbcf747d47c635afecb5a527a99e97",
+    "url": "/dku_festival/static/media/festa2.4bcbcf74.jpeg"
+  },
+  {
+    "revision": "df613892e5cf6cf8241c393bf38e5bda",
+    "url": "/dku_festival/static/media/gf.df613892.png"
+  },
+  {
+    "revision": "8812f4712b21557c809a657bd45acf1e",
+    "url": "/dku_festival/static/media/ha.8812f471.jpg"
+  },
+  {
+    "revision": "bdd5aa1ef174a03b926b5d88f5febc3e",
+    "url": "/dku_festival/static/media/iu.bdd5aa1e.jpg"
+  },
+  {
+    "revision": "d48aa4e0da99188caba7ab3f2d5588da",
+    "url": "/dku_festival/static/media/tt.d48aa4e0.jpeg"
+  }
+]);
