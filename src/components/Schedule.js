924db0e90405af5e6aa8019cc8defdd096f87cfc
InKyoJeong <viapolar@naver.com>
2020-03-08 13:47:23 +0900

    docs: Add button and content to Schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,44 +2,74 @@ import React from "react";
 import styled from "styled-components";
 import { DAY1, MINI_GROUND } from "../data/ScheduleData";
 
-function Schedule() {
-  return (
-    <AllContainer id="schedule">
-      <TitleContainer>
-        <Title>일정</Title>
-      </TitleContainer>
-      <DayContainer>
-        <Day>5/11 (월)</Day>
-        <ContentContainer>
-          <InfoContainer>
-            <Info>대운동장 특설무대 운영시간표</Info>
-          </InfoContainer>
-          <Content>
-            {DAY1.map(data => (
-              <S key={data.id}>
-                <ST>{data.time}</ST>
-                <SText>{data.content}</SText>
-              </S>
-            ))}
-          </Content>
-        </ContentContainer>
-        <ContentContainer>
-          <InfoContainer>
-            <Info>학생회관 소공연장</Info>
-          </InfoContainer>
-          <Content>
-            {MINI_GROUND.map(data => (
-              <S key={data.id}>
-                <ST>{data.time}</ST>
-                <SText>{data.content}</SText>
-              </S>
-            ))}
-          </Content>
-        </ContentContainer>
-      </DayContainer>
-    </AllContainer>
-  );
-}
+const Schedule = () => (
+  <AllContainer id="schedule">
+    <TitleContainer>
+      <Title>일정</Title>
+    </TitleContainer>
+
+    <ButtonContainer>
+      <Button>{"◀️"}</Button>
+      <Button>{"▶️"}</Button>
+    </ButtonContainer>
+
+    <DayContainer>
+      <Day>5/11 (월)</Day>
+      <ContentContainer>
+        <InfoContainer>
+          <Info>대운동장 특설무대 운영시간표</Info>
+        </InfoContainer>
+        <Content>
+          {DAY1.map(data => (
+            <S key={data.id}>
+              <ST>{data.time}</ST>
+              <SC>{data.content}</SC>
+            </S>
+          ))}
+        </Content>
+      </ContentContainer>
+
+      <ContentContainer>
+        <InfoContainer>
+          <Info>학생회관 소공연장</Info>
+        </InfoContainer>
+        <Content>
+          {MINI_GROUND.map(data => (
+            <S key={data.id}>
+              <ST>{data.time}</ST>
+              <SC>{data.content}</SC>
+            </S>
+          ))}
+        </Content>
+      </ContentContainer>
+
+      <ContentContainer>
+        <InfoContainer>
+          <Info>기타 프로그램</Info>
+        </InfoContainer>
+        <Extra>
+          <SC>바자회 : 대운동장 중앙부스 옆</SC>
+          <SC>푸드트럭 : 학생회관 1층 입구 주변</SC>
+          <SC>출발 드림팀 : 에어 바운스 설치(대운동장)</SC>
+          <SC>오락기 및 인형뽑기 : (중앙부스)</SC>
+        </Extra>
+      </ContentContainer>
+    </DayContainer>
+  </AllContainer>
+);
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Button = styled.button`
+  font-size: 40px;
+`;
+
+const Extra = styled.div`
+  flex-direction: column;
+  display: flex;
+`;
 const DayContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -65,7 +95,7 @@ const S = styled.div`
 const ST = styled.div`
   padding-right: 10px;
 `;
-const SText = styled.div``;
+const SC = styled.div``;
 const Content = styled.div`
   border: 2px solid blue;
   padding: 5px 5px;
@@ -76,13 +106,11 @@ const ContentContainer = styled.div`
   align-items: center;
   justify-content: center;
   padding: 10px 0px;
-  font-size: 45px;
-
+  font-size: 35px;
   @media screen and (max-width: 480px) {
     font-size: 25px;
     padding-left: 0px;
   }
-  /* background-color: red; */
 `;
 const Title = styled.div`
   display: flex;
